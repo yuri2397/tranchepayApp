b1272df026a38f00ffe767df5a2f5083b616850b
unknown <aasy.ndiaye@univ-thies.sn>
2022-03-03 15:57:00 +0000

    install nz-zorro

--- admin_app/src/app/app.module.ts
+++ admin_app/src/app/app.module.ts
@@ -11,6 +11,15 @@ import { CommercantComponent } from './pages/commercant/commercant.component';
 import { UsersComponent } from './pages/users/users.component';
 import { ParametreComponent } from './pages/parametre/parametre.component';
 import { CommandesComponent } from './pages/commandes/commandes.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { fr_FR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(fr);
 
 @NgModule({
   declarations: [
@@ -26,9 +35,12 @@ import { CommandesComponent } from './pages/commandes/commandes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
