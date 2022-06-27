17665a4bc6b262a2ab4ad69a30e7c08103d0fbd8
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-06-27 17:55:11 +0000

    cheikh tidiane gueye

--- app/src/app/shared/component/documentation/documentation.module.ts
+++ app/src/app/shared/component/documentation/documentation.module.ts
@@ -4,18 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DocumentationComponent } from 'src/app/shared/component/documentation/documentation.component';
 import { DocumentationServicesComponent } from '../documentation-services/documentation-services.component';
 import { DocumentationRoutingModule } from './documentation-routing.module';
-import { DocumentationPresentationComponent } from '../documentation-presentation/documentation-presentation.component';
-import { DocumentationApiComponent } from '../documentation-api/documentation-api.component';
-import { DocumentationCommercantComponent } from '../documentation-commercant/documentation-commercant.component';
-import { DocumentationParticulierComponent } from '../documentation-particulier/documentation-particulier.component';
 
 @NgModule({
   declarations: [
-    DocumentationPresentationComponent,
     DocumentationServicesComponent,
-    DocumentationApiComponent,
-    DocumentationCommercantComponent,
-    DocumentationParticulierComponent
   ],
   imports: [
     CommonModule,
