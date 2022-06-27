cf8913f51046af947ef6ed2f809bce634e6a92ed
cheikh0101 <ctidiane.gueye1@univ-thies.sn>
2022-06-27 11:09:14 +0000

    cheikh tidiane gueye

--- app/src/app/modules/view/view-routing.module.ts
+++ app/src/app/modules/view/view-routing.module.ts
@@ -3,10 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { ClientComponent } from 'src/app/shared/component/client/client.component';
 import { CommercantComponent } from 'src/app/shared/component/commercant/commercant.component';
 import { ContactComponent } from 'src/app/shared/component/contact/contact.component';
-import { DocumentationApiComponent } from 'src/app/shared/component/documentation-api/documentation-api.component';
-import { DocumentationCommercantComponent } from 'src/app/shared/component/documentation-commercant/documentation-commercant.component';
-import { DocumentationParticulierComponent } from 'src/app/shared/component/documentation-particulier/documentation-particulier.component';
-import { DocumentationServicesComponent } from 'src/app/shared/component/documentation-services/documentation-services.component';
 import { DocumentationComponent } from 'src/app/shared/component/documentation/documentation.component';
 import { IndexComponent } from 'src/app/shared/component/index/index.component';
 import { ViewComponent } from './view.component';
@@ -17,10 +13,17 @@ const routes: Routes = [
   { path: 'page-client', component: ClientComponent },
   { path: 'page-contact', component: ContactComponent },
   { path: 'page-documentation', component: DocumentationComponent },
-  { path: 'page-documentation-services', component: DocumentationServicesComponent },
-  { path: 'page-documentation-api', component: DocumentationApiComponent },
-  { path: 'page-documentation-commercant', component: DocumentationCommercantComponent },
-  { path: 'page-documentation-particulier', component: DocumentationParticulierComponent },
+  // {
+  //   path: 'page-documentation',
+  //   component: ViewComponent,
+  //   children: [
+  //     {
+  //       path: '',
+  //       loadChildren: () =>
+  //         import('./modules/view/view.module').then((m) => m.ViewModule),
+  //     },
+  //   ],
+  // },
 ];
 
 @NgModule({
