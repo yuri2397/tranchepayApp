1d5e8ffe53aa16ed010e09085c86f0215a9b5041
yuri2397 <diawlemor97@gmail.com>
2022-09-14 21:50:22 +0000

    SOME CHANGE

--- api/app/Http/Controllers/PayementIPN.php
+++ api/app/Http/Controllers/PayementIPN.php
@@ -41,6 +41,10 @@ class PayementIPN extends Controller
                     $versement->save();
 
                     if ($padding->type == "fp") {
+                        $log = new Log();
+                        $log->text = "WAVE";
+                        $log->log = "FP TESTE";
+                        $log->save();
                         $compte = Compte::whereBoutiqueId($commande->boutique_id)->first();
                         $compte->solde = $compte->solde + $commande->prix_total;
                         $compte->save();
