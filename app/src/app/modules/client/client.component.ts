b87496307930c42d09a1b20d5287a6ce38682026
yuri2397 <diawlemor97@gmail.com>
2022-09-25 15:59:47 +0000

    SOME CHANGE

--- app/src/app/modules/client/client.component.ts
+++ app/src/app/modules/client/client.component.ts
@@ -1,16 +1,17 @@
-import { Commande } from 'src/app/models/commande';
-import { Router } from '@angular/router';
-import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { ClientService } from './../../services/client.service';
-import { Component, OnInit } from '@angular/core';
-import { Client } from 'src/app/models/client';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { AuthService } from 'src/app/services/auth.service';
+import { NzModalService } from 'ng-zorro-antd/modal'
+import { Commande } from 'src/app/models/commande'
+import { Router } from '@angular/router'
+import { NzNotificationService } from 'ng-zorro-antd/notification'
+import { ClientService } from './../../services/client.service'
+import { Component, OnInit } from '@angular/core'
+import { Client } from 'src/app/models/client'
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { AuthService } from 'src/app/services/auth.service'
 declare interface MenuClass {
-  navbar: string;
-  icon: string;
-  btnConnexion: string;
-  btnInscription: string;
+  navbar: string
+  icon: string
+  btnConnexion: string
+  btnInscription: string
 }
 
 @Component({
@@ -19,74 +20,86 @@ declare interface MenuClass {
   styleUrls: ['./client.component.scss'],
 })
 export class ClientComponent implements OnInit {
-  isLoad = true;
-  selectedCommande!: Commande;
-  client!: Client;
-  isVisible = false;
-  validateForm!: FormGroup;
-  message = 'Premier versement:';
-  isFirst = false;
+  isLoad = true
+  selectedCommande!: Commande
+  client!: Client
+  isVisible = false
+  validateForm!: FormGroup
+  message = 'Premier versement:'
+  isFirst = false
   collapse: MenuClass = {
     navbar: 'navbar',
     icon: 'bi mobile-nav-toggle bi-list text-dark-green',
     btnConnexion: 'text-dark-green',
     btnInscription:
       'btn btn-sm btn-new-account-home mx-3 px-2 py-1 text-whitebtn btn-sm btn-new-account mx-3 px-2 py-1 text-dark-green active',
-  };
-  menuState!: MenuClass;
+  }
+  menuState!: MenuClass
   expended: MenuClass = {
     navbar: 'navbar navbar-mobile',
     icon: 'bi mobile-nav-toggle bi-x',
     btnConnexion:
       'btn btn-sm btn-new-account-home my-sm-1 mx-3 px-2 py-1 text-white',
     btnInscription: 'btn active-btn mx-3 py-1',
-  };
-  isCollapse!: boolean;
+  }
+  isCollapse!: boolean
 
-  isCollapsed = false;
-  title = 'TRANCHEPAY';
+  isCollapsed = false
+  title = 'TRANCHEPAY'
   constructor(
     private router: Router,
     private clientService: ClientService,
-    private authService: AuthService
-  ) { }
+    private authService: AuthService,
+    private modal: NzModalService,
+  ) {}
 
   logout() {
-    this.authService.logout();
-    this.router.navigate(['/auth']);
+    this.modal.confirm({
+      nzTitle: 'Se déconnecter',
+      nzContent:
+        '<p class="lead bold">Voulez-vous vraiment vous deconnecter?</p>',
+      nzOkText: 'Deconnecter',
+      nzCancelText: 'Annuler',
+      nzCentered: true,
+      nzStyle: {},
+      nzOnOk: () => {
+        this.authService.logout()
+        this.router.navigate(['/auth'])
+      },
+    })
   }
 
   ngOnInit(): void {
-    this.isCollapse = false;
-    this.menuState = this.collapse;
-    this.getClient();
+    this.isCollapse = false
+    this.menuState = this.collapse
+    this.getClient()
   }
 
   getClient() {
-    this.isLoad = true;
+    this.isLoad = true
     this.clientService.findClient().subscribe({
       next: (response) => {
-        console.log("client: \n", response);
-        this.client = response;
-        this.clientService.setClient(this.client);
-        this.isLoad = false;
+        console.log('client: \n', response)
+        this.client = response
+        this.clientService.setClient(this.client)
+        this.isLoad = false
       },
       error: (errors) => {
-        console.error(errors);
+        console.error(errors)
       },
-    });
+    })
   }
 
   menuClicked() {
     if (!this.isCollapse) {
-      this.menuState = this.collapse;
+      this.menuState = this.collapse
     } else {
-      this.menuState = this.expended;
+      this.menuState = this.expended
     }
-    this.isCollapse = !this.isCollapse;
+    this.isCollapse = !this.isCollapse
   }
 
   goto(url: string) {
-    this.router.navigate(['/client/' + url]);
+    this.router.navigate(['/client/' + url])
   }
 }
